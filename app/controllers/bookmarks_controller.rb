cc8c3de8a912d367898f7db896dea2ee483d939b
bernardo campos <berposdzn@gmail.com>
2024-05-23 17:27:16 +0100

    rails watch list 04

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: %i[new create destroy]
+  before_action :set_list, only: %i[new create]
 
   def new
     @bookmark = Bookmark.new
@@ -16,7 +16,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), notice: "Bookmark was successfully destroyed.", status: :see_other
   end
 
   private
